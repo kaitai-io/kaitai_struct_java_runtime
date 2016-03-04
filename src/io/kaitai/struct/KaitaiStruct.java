46b9525f9fd829cb57fe01b5eb80111764c486e7
Mikhail Yakshin <greycat@altlinux.org>
2016-03-04 12:58:13 +0300

    Removed _parent support from KaitaiStruct superclass: we have typesafe per-class _parents now; added proper _io getter

--- src/io/kaitai/struct/KaitaiStruct.java
+++ src/io/kaitai/struct/KaitaiStruct.java
@@ -20,14 +20,10 @@ import java.io.IOException;
 
 public class KaitaiStruct {
     protected KaitaiStream _io;
-    protected KaitaiStruct _parent;
 
     public KaitaiStruct(KaitaiStream _io) throws IOException {
         this._io = _io;
     }
 
-    public KaitaiStruct(KaitaiStream _io, KaitaiStruct _parent) throws IOException {
-        this._io = _io;
-        this._parent = _parent;
-    }
+    public KaitaiStream _io() { return _io; }
 }
