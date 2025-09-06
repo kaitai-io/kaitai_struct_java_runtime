94768ca819ae5caeab9880df5e7f648ed08eef7e
Petr Pucil <petr.pucil@seznam.cz>
2025-09-06 16:19:25 +0200

    ConsistencyError: swap params to match the error message
    See https://github.com/kaitai-io/kaitai_struct_python_runtime/commit/690d7008c0cd868130662cefc69827f37c2f5bb8

--- src/main/java/io/kaitai/struct/ConsistencyError.java
+++ src/main/java/io/kaitai/struct/ConsistencyError.java
@@ -25,18 +25,18 @@ package io.kaitai.struct;
 
 public class ConsistencyError extends RuntimeException {
     private final String id;
-    private final Object actual;
     private final Object expected;
+    private final Object actual;
 
-    public ConsistencyError(String id, Object actual, Object expected) {
+    public ConsistencyError(String id, Object expected, Object actual) {
         super("Check failed: " + id + ", expected: " + expected + ", actual: " + actual);
 
         this.id = id;
-        this.actual = actual;
         this.expected = expected;
+        this.actual = actual;
     }
 
     public String id() { return id; }
-    public Object actual() { return actual; }
     public Object expected() { return expected; }
+    public Object actual() { return actual; }
 }
